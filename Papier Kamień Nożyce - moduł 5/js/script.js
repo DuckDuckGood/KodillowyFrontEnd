ccf99e3b9ca9ac20dded128093e8f2c70af4f2a5
Carlos <karol.dziedziel@duck.com>
2023-07-06 23:19:39 +0200

    Removed console.log

--- Papier Kamień Nożyce - moduł 5/js/script.js
+++ Papier Kamień Nożyce - moduł 5/js/script.js
@@ -24,7 +24,6 @@ function getItem(item) {
 }
 
 function getResult(playerItem, jsItem) {
-    console.log(`${playerItem} ${jsItem}`);
     if (playerItem === jsItem) {
         return 'Remis!';
     }
