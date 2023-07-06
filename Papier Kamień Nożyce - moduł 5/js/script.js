80efc84aa011c5500707f28b6c3ca586c7ee45e8
Carlos <karol.dziedziel@duck.com>
2023-07-06 23:18:30 +0200

    Moduł 5

---
+++ Papier Kamień Nożyce - moduł 5/js/script.js
@@ -0,0 +1,40 @@
+function fight(item) {
+    const random = randomize();
+    const items = ['stone', 'paper', 'scissors'];
+    const randomizedItem = items[random];
+    let response = 'Wybór Twojego rywala';
+    response = response + getItem(randomizedItem) + getResult(item, randomizedItem);
+
+    document.getElementById('response').innerHTML = response;
+}
+
+function randomize() {
+    let random = Math.floor(Math.random() * 1000);
+    return random % 3;
+}
+
+function getItem(item) {
+    if (item === 'stone') {
+        return '<i class="fa-regular fa-gem"></i>';
+    }
+    if (item === 'paper') {
+        return '<i class="fa-solid fa-toilet-paper"></i>';
+    }
+    return '<i class="fa-solid fa-scissors"></i>';
+}
+
+function getResult(playerItem, jsItem) {
+    console.log(`${playerItem} ${jsItem}`);
+    if (playerItem === jsItem) {
+        return 'Remis!';
+    }
+
+    if (
+        (playerItem === 'scissors' && jsItem === 'stone')
+        || (playerItem === 'stone' && jsItem === 'paper')
+        || (playerItem === 'paper' && jsItem === 'scissors')
+    ) {
+        return 'Niestety, przegrywasz :(';
+    }
+    return 'Wygrywasz!';
+}
