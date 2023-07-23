6008c36255a55751c7229a78c11f4eecaa5543bb
Carlos <karol.dziedziel@duck.com>
2023-07-23 13:32:14 +0200

    Moduł 6.3

---
+++ Praktyka - moduł 6/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+{
+    const eventHandler = function(event) {
+        event.preventDefault();
+        const clicked = this;
+        const activeElements = document.getElementsByClassName('active');
+        Object.entries(activeElements).forEach(el => el[1].classList.remove('active'));
+
+        const id = clicked.getAttribute('href').replace('#', '');
+
+        clicked.classList.add('active');
+        document.getElementById(id).classList.add('active');
+    }
+
+    const selectors = document.querySelectorAll('li > a');
+    selectors.forEach(selector => selector.addEventListener('click', eventHandler));
+}
