13da078c0e865bedf3c7c85b63bfa6de3284e926
Carlos <karol.dziedziel@duck.com>
2023-07-25 18:17:23 +0200

    7.1

--- Praktyka - moduł 6/js/script.js
+++ Praktyka - moduł 6/js/script.js
@@ -1,37 +1,31 @@
 'use strict';
 {
-    const post = '.post';
-    const postTitle = '.post-title';
-    const titles = '.titles';
+  const post = '.post';
+  const postTitle = '.post-title';
+  const titles = '.titles';
 
-    const eventHandler = function(event) {
-        event.preventDefault();
-        const clicked = this;
-        const activeElements = document.getElementsByClassName('active');
-        Object.entries(activeElements).forEach(el => el[1].classList.remove('active'));
+  const eventHandler = function(event) {
+    event.preventDefault();
+    const clicked = this;
+    const activeElements = document.getElementsByClassName('active');
+    Object.entries(activeElements).forEach(el => el[1].classList.remove('active'));
 
-        const id = clicked.getAttribute('href').replace('#', '');
+    const id = clicked.getAttribute('href').replace('#', '');
 
-        clicked.classList.add('active');
-        document.getElementById(id).classList.add('active');
-    }
+    clicked.classList.add('active');
+    document.getElementById(id).classList.add('active');
+  };
 
-    const selectors = document.querySelectorAll('.titles > li > a');
-    selectors.forEach(selector => selector.addEventListener('click', eventHandler));
+  document.querySelector(titles).innerHTML = '';
 
-    function generateTitles() {
-        removeOldTitles();
-        const articles = document.querySelectorAll(post);
-        Object.values(articles).forEach(article => {
-            const articleId = article.getAttribute('id');
-            const articleTitle = article.querySelector(postTitle).innerHTML;
-            
-            const link = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
-            document.querySelector(titles).insertAdjacentHTML('beforeend', link);
-        });
-    }
+  const articles = document.querySelectorAll(post);
+  Object.values(articles).forEach(article => {
+    const articleId = article.getAttribute('id');
+    const articleTitle = article.querySelector(postTitle).innerHTML;
+    const link = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
+    document.querySelector(titles).insertAdjacentHTML('beforeend', link);
+  });
 
-    function removeOldTitles() {
-        document.querySelector(titles).innerHTML = '';
-    }
+  const selectors = document.querySelectorAll('.titles > li > a');
+  selectors.forEach(selector => selector.addEventListener('click', eventHandler));
 }
