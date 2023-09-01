8c4679c9c9f0b9eff90b8406fd32ee1aaf17e5ff
Carlos <karol.dziedziel@duck.com>
2023-09-01 13:20:30 +0200

    11.5

--- Pizzeria/src/js/helpers.js
+++ Pizzeria/src/js/helpers.js
@@ -7,7 +7,7 @@ export const connectionSettings = {
     defaultMax: 9,
   },
   db: {
-    url: 'http://localhost:3131',
+    url: `//${window.location.hostname}${window.location.hostname === 'localhost' ? ':3131' : ''}`,
     products: 'products',
     orders: 'orders',
     product: 'product',
