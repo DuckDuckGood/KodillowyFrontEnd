a0c2cda83daeee0bed2277629397f4139137ebd8
Carlos <karol.dziedziel@duck.com>
2023-11-28 21:37:35 +0100

    25.1

--- DbServer/server.js
+++ DbServer/server.js
@@ -3,7 +3,7 @@ const runApp = require('./serverUtils');
 
 const dbConnection = () => {
 
-  mongoose.connect('mongodb+srv://soma:Parapapapa123@siemanko.01aosln.mongodb.net/?retryWrites=true&w=majority')
+  mongoose.connect(`mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PSSWD}@siemanko.01aosln.mongodb.net/?retryWrites=true&w=majority`)
     .then(db => runApp(db.connection))
     .catch(err => {
       console.log('Error!', err);
