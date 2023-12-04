0cf98f5557ce3bca197bfd9a1c2a03b5f40d45d7
Carlos <karol.dziedziel@duck.com>
2023-12-04 21:42:35 +0100

    19.6 no cors

--- WaiterApp/server.js
+++ WaiterApp/server.js
@@ -6,19 +6,9 @@ const cors = require('cors');
 const app = express();
 const appPort = process.env.PORT || 3131;
 
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, '/build')));
-
-app.use(cors({
-  origin: `*`,
-  methods: 'GET,POST,DELETE,PUT',
-  optionsSuccessStatus: 200,
-}));
-
-app.options('*', cors({
-  origin: `*`,
-  methods: 'GET,POST,DELETE,PUT',
-  optionsSuccessStatus: 200,
-}));
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
